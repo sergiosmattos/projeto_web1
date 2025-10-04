4aa0859dbb8483effdaacd80f2e0f6a0392b2ddc
KenzoFukushima <20241ctb0100081@estudantes.ifpr.edu.br>
2025-10-04 11:38:09 -0300

    modificando deshBoardAdmin

--- dashboardAdmin.php
+++ dashboardAdmin.php
@@ -3,18 +3,29 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="admin.css">
+    <link rel="stylesheet" href="css/reset.css">
+    <link rel="stylesheet" href="css/admin.css">
+
     <title>Document</title>
 </head>
 <body>
     <section class="topo">
         <div class="logo">
-            <img src="img/logo_geek.png" alt="logo geek artefacts">
-
+            <img src="img/logo_geek.png" class="iconLogo" alt="logo geek artefacts">
             <h1>Geek Artefacts</h1>
         </div>
-
-        <img src="img/icon_user_roxo.svg" alt="IconUsuario">
+        <img src="img/icon_user_branco.svg" class="iconUser" alt="IconUsuario">
     </section>
+
+    <div class="sidebar">
+        <a href="#">Leilões</a>
+        <a href="#">Usuários</a>
+        <a href="#">Categorias</a>
+    </aside>
+
+    <main>
+        <h1>Gerenciar Leilões</h1>
+        <!-- Aqui vem o conteúdo -->
+    </main>
 </body>
 </html>
