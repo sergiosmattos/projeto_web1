13753bacd2b60db9868dc5ed4d21907edf643155
KenzoFukushima <20241ctb0100081@estudantes.ifpr.edu.br>
2025-10-06 17:55:03 -0300

    adicionando Usuario

--- registrarUsuario.php
+++ registrarUsuario.php
@@ -7,18 +7,19 @@ require __DIR__ . "src/Repositorio/conexaoBD.php";
 $usuarioRepositorio = new UsuarioRepositorio($pdo);
 
 $nome = trim($_POST['nome'] ?? '');
-$dataNascimento = 
+$tipo = trim($_POST['tipo'] ?? 'User');
+$dataNascimento = trim($_POST['dataNascimento'] ?? '');
 $email = trim($_POST['email'] ?? '');
 $senha = trim($_POST['senha'] ?? '');
 
 
-if ($nome === '' || dataNascimento === '' || $email === '' || $senha === '') {
+if ($nome === '' || dataNascimento === '' || $email === '' || $senha === '' || dataNascimento === '') {
     header("Location: registrar.php?erro=campos");
     exit;
 }
 
 $senhaHash = password_hash($senha, PASSWORD_DEFAULT);
-$stmt = $pdo->prepare('INSERT INTO tbUsuarios (nome, tipo, email, senha) VALUES (?, ?, ?, ?)');
+$stmt = $pdo->prepare('INSERT INTO tbUsuario (nome_usuario, tipo_usuario, email_usuario, senha, data) VALUES (?, ?, ?, ?)');
 $stmt->execute([$nome, $perfil, $email, password_hash($senha, PASSWORD_DEFAULT)]);
 
 header("Location: login.php?novo=1");
